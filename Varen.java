c0cc7266df20f0f9343f70e9c18efcdf2f939ab8
egorluk2003 <44319573+egorluk2003@users.noreply.github.com>
2018-10-27 11:34:22 +0300

    Add files via upload

---
+++ Varen.java
@@ -0,0 +1,13 @@
+
+public class Varen {
+
+	public Varen() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
